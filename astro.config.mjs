ad86039572a43171ab436af5723756f7b174abc8
Erin O'Connell <erin@system76.com>
2026-05-21 17:25:53 +0000

    (*) add proper relative link support for markdown files in collections

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,25 +2,31 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import rehypeAstroRelativeMarkdownLinks from 'astro-rehype-relative-markdown-links';
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					items: [{ autogenerate: { directory: 'reference' } }],
-				},
-			],
-		}),
-	],
+    integrations: [
+        starlight({
+            title: 'My Docs',
+            social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+            sidebar: [
+                {
+                    label: 'Docs',
+                    items: [{ autogenerate: { directory: 'docs' } }],
+                },
+            ],
+        }),
+    ],
+    site: 'http://localhost:4321',
+    image: {
+        service: {
+            entrypoint: './src/avifImageService.mjs',
+        },
+        layout: "constrained",
+        responsiveStyles: true
+    },
+    markdown: {
+        rehypePlugins: [[rehypeAstroRelativeMarkdownLinks, { collectionBase: false }]],
+      },
 });

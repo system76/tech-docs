fefcb83dd53f59b0299763858ca96a9fac5fe300
Erin O'Connell <erin@system76.com>
2026-06-03 20:18:00 +0000

    (*) do not process svg's under sharp pipeline or wrap them in links since they go unoptimized

--- src/avifImageService.mjs
+++ src/avifImageService.mjs
@@ -1,11 +1,16 @@
 import sharpService from 'astro/assets/services/sharp';
+import sharp from 'sharp';
+
+sharp.concurrency(1);
 
 export default {
-	...sharpService,
-	validateOptions(options, config) {
-		if (!options.format) {
-			options.format = 'avif';
-		}
-		return sharpService.validateOptions(options, config);
-	},
+  ...sharpService,
+
+  // Let SVGs skip validation entirely — sharp can't process them
+  validateOptions(options, config) {
+    if (options.src?.format !== 'svg') {
+        options.format = 'avif';
+    }
+    return sharpService.validateOptions(options, config);
+  }
 };

5baecdf7ec411b87d2bf76c3b24645c927b1eff6
Erin O'Connell <erin@system76.com>
2026-06-08 17:04:53 +0000

    (*) change to specifications and add styles to make pages without sub items look like labels that do have sub items
    (*) type the prettier config file to see proper options and option types
    
    (+) add blame ignore revs for huge format commits

--- .prettierrc.mjs
+++
@@ -1,5 +0,0 @@
-export default {
-  printWidth: 88,
-  plugins: ["prettier-plugin-organize-imports", "prettier-plugin-astro"],
-  overrides: [{ files: "*.astro", options: { parser: "astro" } }],
-};

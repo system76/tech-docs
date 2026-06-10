37933258b46d4f715efde2c18b87219868bb7b47
Erin O'Connell <erin@system76.com>
2026-06-10 00:36:45 +0000

    (*) formatting

--- src/plugins/toc-formatting.ts
+++ src/plugins/toc-formatting.ts
@@ -1,15 +1,16 @@
 import { defineRouteMiddleware } from "@astrojs/starlight/route-data";
 
 export const onRequest = defineRouteMiddleware((context) => {
-  const { starlightRoute } = context.locals;
+    const { starlightRoute } = context.locals;
 
-  if (starlightRoute.toc) { // On pages with a ToC...
+    if (starlightRoute.toc) {
+        // On pages with a ToC...
 
-    // Strip trailing colon from anything that has it
-    for (const tocItem of starlightRoute.toc?.items) {
-      if (tocItem.text.endsWith(":")) {
-        tocItem.text = tocItem.text.slice(0, -1);
-      }
+        // Strip trailing colon from anything that has it
+        for (const tocItem of starlightRoute.toc?.items) {
+            if (tocItem.text.endsWith(":")) {
+                tocItem.text = tocItem.text.slice(0, -1);
+            }
+        }
     }
-  }
 });
